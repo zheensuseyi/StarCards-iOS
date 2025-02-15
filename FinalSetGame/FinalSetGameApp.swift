880159337a13bf0d2fc0abbed0b58f3b0672c7d4
zheen <suseyizheen@gmail.com>
2025-02-14 16:14:44 -0800

    Initial Commit

---
+++ FinalSetGame/FinalSetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalSetGameApp.swift
+//  FinalSetGame
+//
+//  Created by Zheen Suseyi on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalSetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
