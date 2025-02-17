49f3a5a51c14fddf18892999ea94c26285ce2f65
zheen <suseyizheen@gmail.com>
2025-02-17 15:53:32 -0800

    Tons of changes.
    1. Added the cards in a nice lazy v grid
    2. Successfully Rendered the cards
    3. Adding cards function now works

---
+++ FinalSetGame/ShapeInitialization.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeInitialization.swift
+//  FinalSetGame
+//
+//  Created by Zheen Suseyi on 2/17/25.
+//
+
+import SwiftUI
+
+struct ShapeInitialization: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeInitialization()
+}
